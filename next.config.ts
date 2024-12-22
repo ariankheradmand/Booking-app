08dd4731a95f84e9b299ba9410d84ed9b9cce0f7
Arian Kheradmand <ariankheradmand@gmail.com>
2024-12-22 18:31:12 +0330

    admin page added

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,7 @@
-import { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
-/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.plugins?.push({
-        apply: (compiler: any) => {
-          compiler.hooks.beforeRun.tap('DefineSelf', () => {
-            (global as any).self = global;
-          });
-        },
-      });
-    }
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;
